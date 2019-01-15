65162c0c20b5ff4f0d2b3fb38b65af66f6d3d626
Andy Parnell-Smith <andrewparnellsmith@gmail.com>
2019-01-14 16:56:55 -0800

    add to links to projects flip-cards. fix other bugs.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("hamburger").style.display = "none";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("hamburger").style.display = "initial"
   }

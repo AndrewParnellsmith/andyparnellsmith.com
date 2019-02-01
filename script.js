d975c7a000c81180e98007a4b3e2e8ed42dc2000
Andy Parnell-Smith <andrewparnellsmith@gmail.com>
2019-01-31 17:19:31 -0800

    change bg color. bg on header on scroll. and a few misc change.

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  
+  window.addEventListener('scroll', function(e) {
+    document.getElementById("margin").style.background = "#383838"
+    document.getElementById("margin").style.boxShadow = "0 0px 20px 0 #111"
+  })
